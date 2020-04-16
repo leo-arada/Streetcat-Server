7baf80cb4cd281f27b53557c2f0aad8f377f47fb
jiyong <arada3211@gmail.com>
2020-04-17 00:36:55 +0900

    Bug tracking

--- routes/controller/auth.controller.js
+++ routes/controller/auth.controller.js
@@ -6,13 +6,17 @@ const jwt = require('jsonwebtoken');
 exports.logIn = async (req, res, next) => {
   try {
     const { facebookId, name } = req.body;
+    console.log(facebookId, name, 'facebook info', new Date().toISOString())
     const cats = await Cat.find({});
+    // console.log(cats, 'cat info', new Date().toISOString())
     let user = await User.findOne({ facebookId });
+    
     user = user || await new User({ facebookId, name }).save();
+    console.log(user, 'user info', new Date().toISOString())
     const token = jwt.sign({ facebookId, name }, process.env.JWT_KEY, { 
       expiresIn: '3d' 
     });
-
+    console.log(token, 'token', new Date().toISOString())
     res.json({ 
       result: 'ok', 
       user: {
--- routes/middlewares/authentication.js
+++ routes/middlewares/authentication.js
@@ -3,11 +3,14 @@ const createError = require('http-errors');
 
 exports.verifyToken = async (req, res, next) => {
   try {
+    
     let token = req.headers['authorization'];
+    console.log('token in auth', new Date().toISOString())
     token = token.startsWith('Bearer') ? token.slice(7, token.length) : null;
     jwt.verify(token, process.env.JWT_KEY);
     next();
   } catch (error) {
+    console.log('error', new Date().toISOString())
     next(createError(401));
   }
 };
